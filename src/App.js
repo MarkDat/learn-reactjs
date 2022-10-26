9c45e50e28937a303072a3d08a11d50e0909e74f
MarkDat <mark.dat.code@gmail.com>
2022-10-26 16:42:10 +0700

    Add router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import "./App.scss";
 import Header from "./components/Header/Header";
+import { Link } from "react-router-dom";
 
 const App = () => {
   return (
     <div className="app-container">
-      <Header></Header>
+      <Header />
     </div>
   );
 }
